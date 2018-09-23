54e262d954b8c69b3fd2eb02fa1c24d67045c118
peterhoejlundandersen <hilsenit@gmail.com>
2018-09-23 22:19:28 +0200

    two dots in hetamaps

--- app/javascript/heat/app/app.component.ts
+++ app/javascript/heat/app/app.component.ts
@@ -1,43 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { HeatComponent } from './heat.component';
-import { HeatItem } from './heat';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare const h337: any;
 
 @Component({
   selector: 'heat',
-  template: `
-    <button (click)="createAreas()">Start</button>
-    <div id="heatWrapper">
-      <heat_templ *ngFor="let h_item of heat_items" [warmth]="h_item?.warmth" [id]="h_item?.id" [width]="width"></heat_templ>
-    </div>
-  `
+  template: `<div id="heatmap" style="width: 100%; height: 500px"></div>`
 })
 
-export class AppComponent implements OnInit {
-  heat_items: any = [{}] ;
-  innerWidth: any;
-  innerHeight: any;
-  width: number;
-
-  ngOnInit() {
-    this.innerWidth = window.innerWidth;
-    this.innerHeight = window.innerHeight;
-  }
-
-  createAreas() {
-    let width = this.innerWidth;
-    let height = this.innerHeight;
-    let box_w_a_h = 100;
-    let number_of_box_width = width / box_w_a_h;
-    this.width = number_of_box_width;
-    let number_of_box_height = height / box_w_a_h;
-    var total_number_of_boxes = Math.floor(number_of_box_width * number_of_box_height);
-    // I removed this, because it was to slow
-    for(var bi=0; bi < total_number_of_boxes; bi++){
-      let box = new HeatItem
-      box = {id: bi, warmth: bi};
-      this.heat_items.push(box);
-    }
-    console.log(this.heat_items);
+export class AppComponent implements AfterViewInit  {
+   
+  ngAfterViewInit() {
+    const heatmap = h337.create({
+      container: window.document.querySelector('#heatmap')
+    });
+    heatmap.setData({
+      max: 5,
+      data: [{x: 10, y: 15, value: 5}, {x: 100, y: 100, value: 40}]
+    });
   }
 
 }
