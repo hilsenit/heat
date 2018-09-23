746b589a4c3f594ce6e84a1e0c18ea8574095109
peterhoejlundandersen <hilsenit@gmail.com>
2018-09-23 21:08:55 +0200

    to much serverpower for this, so slooow

--- app/javascript/heat/app/app.component.ts
+++ app/javascript/heat/app/app.component.ts
@@ -7,7 +7,7 @@ import { HeatItem } from './heat';
   template: `
     <button (click)="createAreas()">Start</button>
     <div id="heatWrapper">
-      <heat_templ *ngFor="let h_item of heat_items" [warmth]="h_item?.warmth" [id]="h_item?.id"></heat_templ>
+      <heat_templ *ngFor="let h_item of heat_items" [warmth]="h_item?.warmth" [id]="h_item?.id" [width]="width"></heat_templ>
     </div>
   `
 })
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
   heat_items: any = [{}] ;
   innerWidth: any;
   innerHeight: any;
+  width: number;
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
@@ -27,8 +28,10 @@ export class AppComponent implements OnInit {
     let height = this.innerHeight;
     let box_w_a_h = 100;
     let number_of_box_width = width / box_w_a_h;
+    this.width = number_of_box_width;
     let number_of_box_height = height / box_w_a_h;
     var total_number_of_boxes = Math.floor(number_of_box_width * number_of_box_height);
+    // I removed this, because it was to slow
     for(var bi=0; bi < total_number_of_boxes; bi++){
       let box = new HeatItem
       box = {id: bi, warmth: bi};
--- app/javascript/heat/app/heat.component.ts
+++ app/javascript/heat/app/heat.component.ts
@@ -2,15 +2,38 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'heat_templ',
-  template: `<div class="heat-box" [attr.id]="'heatNr' + id" [style.backgroundColor]="setColor(warmth)">{{warmth}}</div>`
+  template: `
+  <div (mouseover)="moreRed()" (mouseleave)="stopColoring()" class="heat-box" [attr.id]="'heatNr' + id" [style.backgroundColor]="color" [style.width]="width + 'px'"></div>
+  `
 })
 
 export class HeatComponent {
   @Input() warmth: number;
   @Input() id: number;
+  @Input() width: number;
+  colorVarmInterval: any;
+  colorCoolInterval: any;
+  va_lev: number = 0;
+  color: string = "rgb(0, 230, 0)";
 
-  setColor(warmth: number): string {
-    return "#" + "ffaadd";
+  moreRed(): void {
+    clearInterval(this.colorCoolInterval);
+    this.colorVarmInterval = setInterval(()=>{
+      this.va_lev += 1;
+      this.color = `rgb(${this.va_lev * 10}, ${250 - this.va_lev * 10},0)`;
+      if(this.va_lev == 25) {
+        clearInterval(this.colorVarmInterval);
+      }
+    }, 150);
   }
-
+  stopColoring() {
+    clearInterval(this.colorVarmInterval);
+    this.colorCoolInterval = setInterval(()=>{
+      this.va_lev -= 1;
+      this.color = `rgb(${this.va_lev * 10}, ${250 - this.va_lev * 10},0)`;
+      if(this.va_lev == 0) {
+        clearInterval(this.colorCoolInterval);
+      }
+    }, 30)
+  }
 }
